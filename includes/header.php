61d1f5670de23b6a8120176f9a9d579807c6c517
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2025-07-03 11:39:22 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -30,10 +30,5 @@ include('config/dbcon.php');
 
 <body >
     <!-- preloader area start -->
-	<div class="preloader" id="preloader">
-		<div class="loader loader-1">
-			<div class="loader-outter"></div>
-			<div class="loader-inner"></div>
-		</div>
-	</div>
+	
 	<!-- preloader area end -->
